361fe0da600487401d48cbe887e824d12c23a7d1
David Haoming Chen <hchen78@sheffield.ac.uk>
2021-08-31 17:51:33 +0100

    Create model.py
    the code to generate the classifier model

---
+++ model.py
@@ -0,0 +1,37 @@
+import numpy as np
+import os
+
+from tflite_model_maker.config import ExportFormat
+from tflite_model_maker import model_spec
+from tflite_model_maker import object_detector
+from tflite_model_maker import image_classifier
+from tflite_model_maker.image_classifier import DataLoader
+
+
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+
+tf.get_logger().setLevel('ERROR')
+from absl import logging
+logging.set_verbosity(logging.ERROR)
+
+
+
+# train_data = DataLoader.from_folder('Dataset/train')
+# print(os.listdir('Dataset/train'))
+# test_data = DataLoader.from_folder('Dataset/test/')
+# validation_data = DataLoader.from_folder('Dataset/valid/')
+# print("hello")
+# spec = model_spec.get('efficientdet_lite2')
+# model = image_classifier.create(train_data)
+# model.evaluate(test_data)
+# model.export(export_dir='.')
+
+
+#efficientdetVV
+spec = model_spec.get('efficientdet_lite2')
+train_data, validation_data, test_data = object_detector.DataLoader.from_csv('C:/Users/David/Desktop/Dissertation3/Dataset/dataset.csv')
+model = object_detector.create(train_data, model_spec=spec, batch_size=250, train_whole_model=True, validation_data=validation_data)
+model.evaluate(test_data)
+model.export(export_dir='.')
+model.evaluate_tflite('model.tflite', test_data)
