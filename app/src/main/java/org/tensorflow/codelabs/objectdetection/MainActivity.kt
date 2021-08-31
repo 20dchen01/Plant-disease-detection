5ead269136c7253438aa770655a86d03ebbb741f
David Haoming Chen <hchen78@sheffield.ac.uk>
2021-08-31 16:52:27 +0100

    Initial commit

---
+++ app/src/main/java/org/tensorflow/codelabs/objectdetection/MainActivity.kt
@@ -0,0 +1,299 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * David Chen 2021 Plant Disease detection
+ */
+
+package org.tensorflow.codelabs.objectdetection
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.*
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier.ImageClassifierOptions
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.max
+import kotlin.math.min
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    companion object {
+        const val TAG = "TFLite - ODT"
+        const val REQUEST_IMAGE_CAPTURE: Int = 1
+        val IMAGE_REQUEST_CODE = 100
+
+    }
+
+    private lateinit var captureImageFab: Button
+    private lateinit var button: Button
+    private lateinit var inputImageView: ImageView
+    private lateinit var imgSampleOne: ImageView
+    private lateinit var imgSampleTwo: ImageView
+    private lateinit var imgSampleThree: ImageView
+    private lateinit var tvPlaceholder: TextView
+    private lateinit var resultsOutputArea: TextView
+    private lateinit var currentPhotoPath: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        captureImageFab = findViewById(R.id.captureImageFab)
+        inputImageView = findViewById(R.id.imageView)
+        imgSampleOne = findViewById(R.id.imgSampleOne)
+        imgSampleTwo = findViewById(R.id.imgSampleTwo)
+        imgSampleThree = findViewById(R.id.imgSampleThree)
+        tvPlaceholder = findViewById(R.id.tvPlaceholder)
+        resultsOutputArea = findViewById(R.id.resultsOutputArea)
+        button = findViewById(R.id.btn_pick_img)
+
+        button.setOnClickListener(this)
+        captureImageFab.setOnClickListener(this)
+        imgSampleOne.setOnClickListener(this)
+        imgSampleTwo.setOnClickListener(this)
+        imgSampleThree.setOnClickListener(this)
+    }
+    private fun pickImageGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            setViewAndDetect(getCapturedImage())
+        }
+        if (requestCode== IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
+            inputImageView.setImageURI(data?.data)
+            val bmap: Bitmap = getBitmapFromView(inputImageView)
+            setViewAndDetect(bmap)
+        }
+
+    }
+    open fun getBitmapFromView(view: View): Bitmap {
+        var bitmap =
+            Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        var canvas = Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
+    }
+    /**
+     * onClick(v: View?)
+     *      Detect touches on the UI components
+     */
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.captureImageFab -> {
+                try {
+                    dispatchTakePictureIntent()
+                } catch (e: ActivityNotFoundException) {
+                    Log.e(TAG, e.message.toString())
+                }
+            }
+            R.id.btn_pick_img -> {
+                pickImageGallery()
+            }
+            R.id.imgSampleOne -> {
+                setViewAndDetect(getSampleImage(R.drawable.applecedarrust))
+            }
+            R.id.imgSampleTwo -> {
+                setViewAndDetect(getSampleImage(R.drawable.tomatoearlyblight4))
+            }
+            R.id.imgSampleThree -> {
+                setViewAndDetect(getSampleImage(R.drawable.tomatoyellowcurlvirus4))
+            }
+        }
+    }
+
+    /**
+     * runObjectDetection(bitmap: Bitmap)
+     *      TFLite Object Detection function
+     */
+    private fun runObjectDetection(bitmap: Bitmap) {
+        val image = TensorImage.fromBitmap(bitmap)
+        val options = ImageClassifierOptions.builder().setMaxResults(1).build()
+        val imageClassifier = ImageClassifier.createFromFileAndOptions(this, "model.tflite", options)
+
+        val results = imageClassifier.classify(image)
+        val resultToDisplay = results.map {
+            // Get the top-1 category and craft the display text
+            val category = it.categories.first()
+
+            val text = "${category.label}:  ${category.score.times(100).toInt()}% "
+
+            // Create a data object to display the detection result
+            text
+        }
+// Draw the detection result on the bitmap and show it.
+        resultsOutputArea.setText(resultToDisplay.get(0))
+    }
+
+
+
+    /**
+     * setViewAndDetect(bitmap: Bitmap)
+     *      Set image to view and call object detection
+     */
+    private fun setViewAndDetect(bitmap: Bitmap) {
+        // Display capture image
+        inputImageView.setImageBitmap(bitmap)
+        tvPlaceholder.visibility = View.INVISIBLE
+
+        // Run ODT and display result
+        lifecycleScope.launch(Dispatchers.Default) { runObjectDetection(bitmap) }
+    }
+
+    /**
+     * getCapturedImage():
+     *      Decodes and crops the captured image from camera.
+     */
+    private fun getCapturedImage(): Bitmap {
+        // Get the dimensions of the View
+        val targetW: Int = inputImageView.width
+        val targetH: Int = inputImageView.height
+
+        val bmOptions = BitmapFactory.Options().apply {
+            // Get the dimensions of the bitmap
+            inJustDecodeBounds = true
+
+            BitmapFactory.decodeFile(currentPhotoPath, this)
+
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+
+            // Determine how much to scale down the image
+            val scaleFactor: Int = max(1, min(photoW / targetW, photoH / targetH))
+
+            // Decode the image file into a Bitmap sized to fill the View
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inMutable = true
+        }
+        val exifInterface = ExifInterface(currentPhotoPath)
+        val orientation = exifInterface.getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_UNDEFINED
+        )
+
+        val bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> {
+                rotateImage(bitmap, 90f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_180 -> {
+                rotateImage(bitmap, 180f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_270 -> {
+                rotateImage(bitmap, 270f)
+            }
+            else -> {
+                bitmap
+            }
+        }
+    }
+
+    /**
+     * getSampleImage():
+     *      Get image form drawable and convert to bitmap.
+     */
+    private fun getSampleImage(drawable: Int): Bitmap {
+        return BitmapFactory.decodeResource(resources, drawable, BitmapFactory.Options().apply {
+            inMutable = true
+        })
+    }
+
+    /**
+     * rotateImage():
+     *     Decodes and crops the captured image from camera.
+     */
+    private fun rotateImage(source: Bitmap, angle: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(angle)
+        return Bitmap.createBitmap(
+            source, 0, 0, source.width, source.height,
+            matrix, true
+        )
+    }
+
+    /**
+     * createImageFile():
+     *     Generates a temporary image file for the Camera app to write to.
+     */
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    /**
+     * dispatchTakePictureIntent():
+     *     Start the Camera app to take a photo.
+     */
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (e: IOException) {
+                    Log.e(TAG, e.message.toString())
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "org.tensorflow.codelabs.objectdetection.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+}
